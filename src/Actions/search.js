e16848defe9600592269f53b91c2cafe376ae010
Mahmoud Sherif <mahmoud.sherif@bld.ai>
2021-05-21 17:51:34 +0200

    Use axios

--- src/Actions/search.js
+++ src/Actions/search.js
@@ -1,9 +1,13 @@
+import axios from 'axios';
 
 const search = async (searchText) => {
-    // let url = "https://www.googleapis.com/youtube/v3/videos"
-    // let response = await fetch(url)
-    console.log(searchText)
-    let response = ['3FH7pqTP4tY', '1PSIdXMMn7I', 'wwbM63-LFD4', 'rZjciV-pff0', 'GVHighLm3os']
-    return response
+    const response = await axios.get("http://localhost:5000/search", {params: {'q': searchText}})
+    console.table(response)
+    const idsList = []
+    response.data.forEach(video => {
+        idsList.push(video['id']);
+    });
+    // let response = ['3FH7pqTP4tY', '1PSIdXMMn7I', 'wwbM63-LFD4', 'rZjciV-pff0', 'GVHighLm3os']
+    return idsList
 }
 export default search
