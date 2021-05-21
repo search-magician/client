cb4333c448fa3099ca98312e43be6d5c1914c129
Bohoty <omarbohoty@gmail.com>
2021-05-21 15:40:49 +0200

    Add truncating searchText1 in search2

--- src/Actions/search2.js
+++ src/Actions/search2.js
@@ -1,6 +1,8 @@
 const search2 = async (searchText1, searchText2) => {
     // let url = "https://www.googleapis.com/youtube/v3/videos"
     // let response = await fetch(url)
+    let startIdx = searchText1.search("=")
+    searchText1 = searchText1.substring(startIdx + 1)
     console.log(searchText1, searchText2)
     let response = ['3FH7pqTP4tY', '1PSIdXMMn7I', 'wwbM63-LFD4', 'rZjciV-pff0', 'GVHighLm3os']
     return response
